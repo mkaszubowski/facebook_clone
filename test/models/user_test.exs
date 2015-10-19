fa6e3e1218404e83109385d7bd927c98e1a85428
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-19 23:29:05 +0200

    add generated user model

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule FacebookClone.UserTest do
+  use FacebookClone.ModelCase
+
+  alias FacebookClone.User
+
+  @valid_attrs %{crypted_password: "some content", email: "some content"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
---
+++ web/models/user.ex
@@ -0,0 +1,24 @@
+defmodule FacebookClone.User do
+  use FacebookClone.Web, :model
+
+  schema "users" do
+    field :email, :string
+    field :crypted_password, :string
+
+    timestamps
+  end
+
+  @required_fields ~w(email crypted_password)
+  @optional_fields ~w()
+
+  @doc """
+  Creates a changeset based on the `model` and `params`.
+
+  If no params are provided, an invalid changeset is returned
+  with no validation performed.
+  """
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, @required_fields, @optional_fields)
+  end
+end
