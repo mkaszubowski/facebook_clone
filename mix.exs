bfbbed5b2b3b926d341651f772dfb5e4b99d988c
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-21 15:11:02 +0200

    add comeonin

--- mix.exs
+++ mix.exs
@@ -35,7 +35,8 @@ defmodule FacebookClone.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:comeonin, "~> 1.0"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
