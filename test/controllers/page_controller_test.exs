ed69f76b64639ae4c520c3bf1680d0fa73a5fd95
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-22 13:17:35 +0200

    add login link to the homepage

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -6,4 +6,10 @@ defmodule FacebookClone.PageControllerTest do
 
     assert html_response(conn, 200) =~ "Register"
   end
+
+  test "homepage displays 'login' link" do
+    conn = get conn(), "/"
+
+    assert html_response(conn, 200) =~ "Login"
+  end
 end
