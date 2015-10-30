40d9b0107bca680ed930ad9ea43019980c6ca888
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-30 20:39:42 +0100

    add user controller index test

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -1,18 +1,31 @@
 defmodule FacebookClone.UserControllerTest do
   use FacebookClone.ConnCase
 
-  alias FacebookClone.User
-  alias FacebookClone.Repo
-
   import FacebookClone.CreateUser, only: [create_user: 2]
 
+  setup_all do
+    users =
+      1..10
+      |> Enum.map(fn x -> create_user("foo-#{x}@bar.com", "password") end)
+
+    {:ok, users: users}
+  end
+
+  test "GET /users", context do
+    conn = get conn(), "/users/"
+
+    assert conn.status == 302
+
+    context[:users]
+    |> Enum.each(fn user -> assert conn.body =~ user.first_name end)
+  end
 
   test "GET /users/:id/edit" do
     {:ok, user} = create_user("foo@bar.com", "password")
 
     conn = get conn(), "/users/#{user.id}/edit"
 
-    assert html_response(conn, 200) =~ "Edit your profile"
+    assert html_response(conn, 302) =~ "Edit your profile"
   end
 
   test "GET /users/:id/edit displays user edit form" do
@@ -20,6 +33,6 @@ defmodule FacebookClone.UserControllerTest do
 
     conn = get conn(), "/users/#{user.id}/edit"
 
-    assert html_response(conn, 200) =~ ~r/<form.*users.*>/
+    assert html_response(conn, 302) =~ ~r/<form.*users.*>/
   end
 end
