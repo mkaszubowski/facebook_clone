c0f77abc4adc85fa4e67e02a05b9ba1fe27f8618
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-31 23:15:02 +0100

    simulate logging in instead of using mocks for current_user

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -1,46 +1,36 @@
 defmodule FacebookClone.UserControllerTest do
-  use FacebookClone.ConnCase
+  use FacebookClone.ConnCase, async: false
 
-  import Mock
-
-  alias FacebookClone.SessionHandler
   alias FacebookClone.TestHelper
 
   setup do
     users =
-      1..10
-      |> Enum.map(fn(x) -> TestHelper.create_user("foo-#{x}@bar.com", "password", "Foo-#{x}", "Bar-#{x}") end)
+      1..5
+      |> Enum.map(fn(x) -> TestHelper.create_user("foo-#{x}@bar.com", "foobar", "Foo-#{x}", "Bar-#{x}") end)
       |> Enum.map(fn(x) -> elem(x, 1) end)
 
-    {:ok, users: users}
+    user = Enum.at(users, 0)
+
+    session_params =
+      %{"session": %{"email": user.email, "password": "foobar"}}
+
+    conn = post conn(), "/login", session_params
+
+    {:ok, users: users, conn: conn, current_user: user}
   end
 
 
-  test "GET /users", context do
-    with_mock SessionHandler, mock_user_params do
-      conn = get conn(), "/users"
+  test "GET /users", %{users: users, conn: conn} do
+    conn = get conn, "/users"
 
-      context[:users]
-      |> Enum.each(fn(user) -> assert conn.resp_body =~ user.first_name end)
-    end
+    users
+    |> Enum.each(fn(user) -> assert conn.resp_body =~ user.first_name end)
   end
 
-  test "GET /users/:id/edit" do
-    {:ok, user} =
-      TestHelper.create_user("foo@bar.com", "password", "Foo", "Bar")
+  test "GET /users/:id/edit", %{conn: conn, current_user: current_user} do
+    conn = get conn, "/users/#{current_user.id}/edit"
 
-    with_mock SessionHandler, mock_user_params(user) do
-      conn = get conn(), "/users/#{user.id}/edit"
-
-      assert html_response(conn, 200) =~ "Edit your profile"
-      assert html_response(conn, 200) =~ ~r/<form.*users.*>/
-    end
-  end
-
-  defp mock_user_params(user \\ true) do
-    [
-      logged_in?: fn(_conn) -> true end,
-      current_user: fn(_conn) -> user end
-    ]
+    assert html_response(conn, 200) =~ "Edit your profile"
+    assert html_response(conn, 200) =~ ~r/<form.*users.*>/
   end
 end
