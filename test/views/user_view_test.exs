0358dce7a6ae219bfb00d72a0b870a27cb50201d
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-10-31 18:12:23 +0100

    refactor user view test

--- test/views/user_view_test.exs
+++ test/views/user_view_test.exs
@@ -7,14 +7,18 @@ defmodule FacebookClone.UserViewTest do
   import FacebookClone.UserView, only: [full_name: 1]
   import FacebookClone.CreateUser, only: [create_user: 2]
 
-  test "full_name function returns user's full name" do
+  setup do
     {:ok, user} = create_user("foo@bar.com", "password")
     changeset = User.changeset(
       user,
       %{"first_name": "Foo", "last_name": "Bar"},
       :update)
-    {:ok, user} = Repo.update(changeset)
 
-    assert full_name(user) == "Foo Bar"
+    {:ok, user} = Repo.update(changeset)
+    {:ok, user: user}
+  end
+
+  test "full_name function returns user's full name", context do
+    assert full_name(context[:user]) == "Foo Bar"
   end
 end
