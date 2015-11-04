1db3d537c0082f22218ee567c7bd3d4a6ded2173
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-11-04 22:36:59 +0100

    test for user authentication & authorisation in update action

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -6,6 +6,13 @@ defmodule FacebookClone.UserControllerTest do
   alias FacebookClone.Repo
   alias FacebookClone.User
 
+  @update_params %{
+    "user": %{
+      "email": "new@email.com",
+      "first_name": "new_name"
+    }
+  }
+
   setup do
     users =
       1..5
@@ -37,20 +44,34 @@ defmodule FacebookClone.UserControllerTest do
     assert html_response(conn, 200) =~ ~r/<form.*users.*>/
   end
 
-  test "PUT /users/:id", %{conn: conn, current_user: current_user} do
-    params = %{
-      "user": %{
-        "email": "new@email.com",
-        "first_name": "new_name"
-      }
-    }
+  test "PUT /users/:id for the current user",
+    %{conn: conn, current_user: current_user} do
+
     old_email = current_user.email
 
-    put conn, "/users/#{current_user.id}", params
+    put conn, "/users/#{current_user.id}", @update_params
 
     user = Repo.get(User, current_user.id)
 
     assert user.first_name == "new_name"
     assert user.email == old_email
+  end
+
+  test "PUT /users/:id for other users", %{conn: conn, users: users} do
+    not_current_user = Enum.at(users, 4)
+
+    conn = put conn, "/users/#{not_current_user.id}", @update_params
+
+    assert html_response(conn, 302) =~ "redirected"
+    assert redirected_to(conn) == "/"
+  end
+
+  test "PUT /users/:id when not logged in" do
+    {:ok, user} = TestHelper.create_user("foo@bar.com", "foobar")
+
+    conn = put conn(), "/users/#{user.id}", @update_params
+
+    assert html_response(conn, 302) =~ "redirected"
+    assert redirected_to(conn) == "/login"
   end
 end
