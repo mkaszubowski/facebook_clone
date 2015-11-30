e19b55fc40cd5ee607e48b38714d59bb42bc6fb5
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-11-30 23:09:16 +0100

    add remove friend button

--- web/controllers/friendship_controller.ex
+++ web/controllers/friendship_controller.ex
@@ -1,6 +1,7 @@
 defmodule FacebookClone.FriendshipController do
   use FacebookClone.Web, :controller
 
+  alias FacebookClone.Repo
   alias FacebookClone.Friendship
   alias FacebookClone.SessionPlug
   alias FacebookClone.User
@@ -13,49 +14,13 @@ defmodule FacebookClone.FriendshipController do
 
   def index(conn, _params) do
     friends =
-      conn
-      |> current_user
-      |> User.friends
-    invited_by =
-      conn
-      |> current_user
-      |> User.invited_by
+      (current_user(conn) |> Repo.preload(:friends)).friends
 
-    render(conn, "index.html", friends: friends, invited_by: invited_by)
+    render(conn, "index.html", friends: friends, invited_by: [])
   end
 
-  def create(conn, %{"friendship" => friendship}) do
-    params = current_user_friendship_params(conn, friendship)
-    changeset = Friendship.changeset(%Friendship{}, params)
-
-    case Repo.insert(changeset) do
-      {:ok, _friendship} ->
-        conn
-        |> put_flash(:info, "User has been invited to your friends")
-        |> redirect to: user_path(conn, :index)
-      {:error, reason} ->
-        conn
-        |> put_flash(:info, error_messages(reason))
-        |> redirect to: user_path(conn, :index)
-    end
-  end
-
-  def update(conn, %{"friendship" => friendship}) do
-    user_one_id = friendship["user_one_id"]
-    user_two_id = current_user(conn).id
-
-    case Repo.get_by(Friendship, user_one_id: user_one_id, user_two_id: user_two_id) do
-      nil ->
-        conn
-        |> put_flash(:info, "You have not been invited by that user")
-        |> redirect to: friendship_path(conn, :index)
-      friendship ->
-        accept_friendship(conn, friendship)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    friendship = Repo.get(Friendship, id)
+  def delete(conn, %{"friendship" => friendship}) do
+    friendship = Repo.get_by(Friendship, friend_id: friendship["friend_id"])
 
     case Repo.delete(friendship) do
       {:ok, _friendship} ->
@@ -69,31 +34,9 @@ defmodule FacebookClone.FriendshipController do
     end
   end
 
-  defp current_user_friendship_params(conn, friendship) do
-    %{
-      user_one_id: current_user(conn).id,
-      user_two_id: String.to_integer(friendship["user_two_id"])
-    }
-  end
-
-  defp error_messages(changeset) do
-    changeset.errors
-    |> Dict.values
-    |> Enum.join(". ")
-  end
-
-  defp accept_friendship(conn, friendship) do
-    changeset = Friendship.changeset(friendship, %{accepted: true})
-
-    case Repo.update(changeset) do
-      {:ok, _friendship} ->
-        conn
-        |> put_flash(:info, "You have accepted this friend")
-        |> redirect to: friendship_path(conn, :index)
-      _ ->
-        conn
-        |> put_flash(:info, "An error occurred. Try again later.")
-        |> redirect to: friendship_path(conn, :index)
-    end
-  end
+#   defp error_messages(changeset) do
+#     changeset.errors
+#     |> Dict.values
+#     |> Enum.join(". ")
+#   end
 end
--- web/views/friendship_view.ex
+++ web/views/friendship_view.ex
@@ -12,4 +12,14 @@ defmodule FacebookClone.FriendshipView do
       ]
     end
   end
+
+  def remove_friend_button(conn, friend) do
+    form_for conn, friendship_path(conn, :delete, friend),
+             [as: :friendship, method: :delete], fn f ->
+      [
+        (text_input f, :friend_id, type: :hidden, value: friend.id),
+        (submit "Remove", class: "btn btn-primary")
+      ]
+    end
+  end
 end
