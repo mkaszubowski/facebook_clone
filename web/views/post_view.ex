218d6cef5249e2074ede5adf7f6037dc53446c6c
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-07 18:58:02 +0100

    refactor post edit and delete links helpers

--- web/views/post_view.ex
+++ web/views/post_view.ex
@@ -1,15 +1,12 @@
 defmodule FacebookClone.PostView do
   use FacebookClone.Web, :view
 
-  def edit_button(conn, post, current_user) do
+  def post_links(conn, post, current_user) do
     if post.user_id == current_user.id do
-      link "Edit", to: post_path(conn, :edit, post)
-    end
-  end
-
-  def delete_button(conn, post, current_user) do
-    if post.user_id == current_user.id do
-      link "Delete", to: post_path(conn, :delete, post), method: :delete
+      [
+        link("Edit", to: post_path(conn, :edit, post)),
+        link("Delete", to: post_path(conn, :delete, post), method: :delete)
+      ]
     end
   end
 end
