8f08f750e8dc465a06f23e636bfa46ca7729f150
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-13 22:00:54 +0100

    Style invite buttons on users page

--- web/views/user_view.ex
+++ web/views/user_view.ex
@@ -12,20 +12,26 @@ defmodule FacebookClone.UserView do
       current_user,
       [:friends, :friendship_invitations])
 
-    friends_ids =
-      [
-        current_user.friendship_invitations
-        |> Enum.map(fn(invitation) -> invitation.invited_id end),
-        current_user.friends
-        |> Enum.map(fn(friend) -> friend.id end)
-      ]
-      |> List.flatten
+    pending_ids =
+      current_user.friendship_invitations
+      |> Enum.map(fn(invitation) -> invitation.invited_id end)
 
-    unless Enum.member?(friends_ids, user.id), do: invite_form_tag(conn, user)
+    pending = Enum.member?(pending_ids, user.id)
+
+    friends_ids =
+      current_user.friends
+      |> Enum.map(fn(friend) -> friend.id end)
+
+
+    if Enum.member?(friends_ids, user.id) do
+      friend_label
+    else
+      invite_form_tag(conn, user, pending)
+    end
   end
 
 
-  defp invite_form_tag(conn, user) do
+  defp invite_form_tag(conn, user, pending) when pending == false do
     form_for conn, friendship_invitation_path(conn, :create),
       [as: :friendship_invitation], fn f ->
       [
@@ -34,4 +40,19 @@ defmodule FacebookClone.UserView do
       ]
     end
   end
+
+  defp invite_form_tag(conn, user, pending) when pending == true do
+    form_for conn, friendship_invitation_path(conn, :create),
+      [as: :friendship_invitation], fn f ->
+      [
+        (text_input f, :invited_id, type: :hidden, value: user.id),
+        (submit "Invite", class: "btn btn-primary invite-user", disabled: true)
+      ]
+    end
+  end
+
+  defp friend_label do
+    content_tag(:span, "Already friends", class: "friend-label")
+  end
+
 end
