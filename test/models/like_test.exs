4af730ca8366a1e5da09a7503d0519b5735607ab
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-13 12:49:19 +0100

    Add Like model test

---
+++ test/models/like_test.exs
@@ -0,0 +1,33 @@
+defmodule FacebookClone.LikeTest do
+  use FacebookClone.ModelCase
+
+  alias FacebookClone.User
+  alias FacebookClone.Post
+  alias FacebookClone.Like
+  alias FacebookClone.Repo
+
+  alias FacebookClone.TestHelper
+
+  setup do
+    {:ok, user} = TestHelper.create_user("foo@bar.com", "password")
+
+    post_changeset = Post.changeset(%Post{}, %{
+      user_id: user.id, content: "test"})
+
+    {:ok, post} = Repo.insert(post_changeset)
+
+    {:ok, post: post, user: user}
+  end
+
+  test "is invalid without user id", %{post: post} do
+    changeset = Like.changeset(%Like{}, %{post_id: post.id})
+
+    refute changeset.valid?
+  end
+
+  test "is invalid without post id", %{user: user} do
+    changeset = Like.changeset(%Like{}, %{user_id: user.id})
+
+    refute changeset.valid?
+  end
+end
