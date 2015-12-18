b1bf960f9d39cb12f3d5fb06f94b2f265107fbcc
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-18 17:14:48 +0100

    upgrade phoenix and ecto versions

--- web/web.ex
+++ web/web.ex
@@ -18,8 +18,9 @@ defmodule FacebookClone.Web do
 
   def model do
     quote do
-      use Ecto.Model
+      use Ecto.Schema
 
+      import Ecto
       import Ecto.Changeset
       import Ecto.Query, only: [from: 1, from: 2]
     end
@@ -30,6 +31,7 @@ defmodule FacebookClone.Web do
       use Phoenix.Controller
 
       alias FacebookClone.Repo
+      import Ecto
       import Ecto.Model
       import Ecto.Query, only: [from: 1, from: 2]
 
@@ -64,6 +66,7 @@ defmodule FacebookClone.Web do
       use Phoenix.Channel
 
       alias FacebookClone.Repo
+      import Ecto
       import Ecto.Model
       import Ecto.Query, only: [from: 1, from: 2]
     end
