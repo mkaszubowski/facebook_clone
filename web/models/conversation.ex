af46c3a40bcddf8d746ff203fefca1fd28214e7a
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-29 23:23:57 +0100

    Add Conversation.for_user query function

--- web/models/conversation.ex
+++ web/models/conversation.ex
@@ -2,14 +2,13 @@ defmodule FacebookClone.Conversation do
   use FacebookClone.Web, :model
 
   alias FacebookClone.User
-  alias FacebookClone.ConversationUser
+  alias FacebookClone.Conversation
 
   schema "conversations" do
     field :name, :string
 
     timestamps
 
-    has_many :conversation_users, ConversationUser
     belongs_to :user_one, User
     belongs_to :user_two, User
   end
@@ -20,5 +19,13 @@ defmodule FacebookClone.Conversation do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+  end
+
+  def for_user(user) do
+    Conversation
+    |> join(:inner, [c], u1 in User, c.user_one_id == u1.id)
+    |> join(:inner, [c], u2 in User, c.user_two_id == u2.id)
+    |> preload([:user_one, :user_two])
+    |> order_by(desc: :updated_at)
   end
 end
