86d68e2021519e759cad34df523e9721bce7c1f9
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2015-12-30 00:48:49 +0100

    Add link to conversation in conversations index action

--- web/views/conversation_view.ex
+++ web/views/conversation_view.ex
@@ -3,11 +3,24 @@ defmodule FacebookClone.ConversationView do
 
   import FacebookClone.UserView, only: [full_name: 1]
 
-  def other_user(conversation, current_user) do
+  def conversation_link(conn, conversation, current_user) do
+    link(
+      other_user_name(conversation, current_user),
+      to: conversation_path(conn, :show, conversation)
+    )
+  end
+
+  defp other_user(conversation, current_user) do
     if current_user.id == conversation.user_one_id do
       conversation.user_two
     else
       conversation.user_one
     end
+  end
+
+  defp other_user_name(conversation, current_user) do
+    conversation
+    |> other_user(current_user)
+    |> full_name
   end
 end
