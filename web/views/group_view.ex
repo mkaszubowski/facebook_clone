7e2d346d564b3bb9d6e86540484287391736306e
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-01-20 17:49:10 +0100

    Group views styling

--- web/views/group_view.ex
+++ web/views/group_view.ex
@@ -10,4 +10,8 @@ defmodule FacebookClone.GroupView do
     if group.user_id == conn.assigns.current_user_id, do:
       link("Delete", to: group_path(conn, :delete, group), method: :delete)
   end
+
+  def submit_label(conn) do
+    if Regex.match?(~r/edit/, conn.request_path), do: "Update", else: "Create"
+  end
 end
