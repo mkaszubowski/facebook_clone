594c6d9a6354caee02cc9d4168550d92467031ee
Maciej Kaszubowski <maciej@mkaszubowski.pl>
2016-01-29 00:25:53 +0100

    Add accept_link to EventView

--- web/views/event_view.ex
+++ web/views/event_view.ex
@@ -1,3 +1,7 @@
 defmodule FacebookClone.EventView do
   use FacebookClone.Web, :view
+
+  def accept_link(conn, invitation) do
+    link "Accept", to: event_invitation_path(conn, :update, invitation), method: :put
+  end
 end
